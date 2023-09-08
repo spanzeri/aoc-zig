f36018beea30d14a786c638d08f42951edea5859
Samuele Panzeri <samuele.panzeri@gmail.com>
2023-09-08 12:59:38 +0200

    Change the default solution code to be a std.debug.print for extra convenience

--- templates/day.zig
+++ templates/day.zig
@@ -6,9 +6,9 @@ const gpa = gpaimpl.allocator();
 const data = @embedFile("data/input${day}.txt");
 
 pub fn solution1() !void {
-    unreachable;
+    std.debug.print("Solution 1: \n", .{});
 }
 
 pub fn solution2() !void {
-    unreachable;
+    std.debug.print("Solution 2: \n", .{});
 }
